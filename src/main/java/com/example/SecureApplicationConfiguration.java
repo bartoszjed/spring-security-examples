a9a76bc9e79e6000a2fe3de409b10cda28155f38
Robert Firek <robert.firek@gmail.com>
2017-03-21 20:42:36 +0000

    Introduce default security model

--- src/main/java/com/example/SecureApplicationConfiguration.java
+++ src/main/java/com/example/SecureApplicationConfiguration.java
@@ -1,9 +1,16 @@
 package com.example;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecureApplicationConfiguration
+@EnableGlobalMethodSecurity()
+public class SecureApplicationConfiguration extends WebSecurityConfigurerAdapter
 {
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll();
+    }
 }
