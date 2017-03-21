27dd125b288f6bf42aea973a6cab27fa899fb2b4
Robert Firek <robert.firek@gmail.com>
2017-03-21 21:01:08 +0000

    Add in memory authentication

--- src/main/java/com/example/SecureApplicationConfiguration.java
+++ src/main/java/com/example/SecureApplicationConfiguration.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,11 +10,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableGlobalMethodSecurity()
 public class SecureApplicationConfiguration extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests().antMatchers("/secret").authenticated().and().httpBasic().
-                and().
-                authorizeRequests().anyRequest().permitAll();
+        http
+                .authorizeRequests().antMatchers("/secret").hasRole("USER")
+                .and()
+                .authorizeRequests().antMatchers("/top/secret").hasRole("ADMIN")
+                .and().httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("ADMIN");
     }
 }
