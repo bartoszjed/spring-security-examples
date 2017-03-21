45cd99894055b3417fbbb11652346145f9ac99ab
Robert Firek <robert.firek@gmail.com>
2017-03-21 21:07:42 +0000

    Added a custom authentication provider

--- src/main/java/com/example/SecureApplicationConfiguration.java
+++ src/main/java/com/example/SecureApplicationConfiguration.java
@@ -2,10 +2,17 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 @EnableGlobalMethodSecurity()
@@ -22,10 +29,32 @@ public class SecureApplicationConfiguration extends WebSecurityConfigurerAdapter
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .withUser("admin").password("password").roles("ADMIN");
+        auth.authenticationProvider(new CustomAuthenticationProvider());
+    }
+
+    public static class CustomAuthenticationProvider implements AuthenticationProvider {
+
+        @Override
+        public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+            if (isExternalUser(authentication) && "external_password".equals(authentication.getCredentials())) {
+
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        authentication.getPrincipal(),
+                        authentication.getCredentials(),
+                        singletonList(authentication.getPrincipal().equals("external_user") ? new SimpleGrantedAuthority("ROLE_USER") : new SimpleGrantedAuthority("ROLE_ADMIN")));
+                return usernamePasswordAuthenticationToken;
+            }
+            return null;
+        }
+
+        private boolean isExternalUser(Authentication authentication) {
+            return authentication.getPrincipal().equals("external_user") ||
+                    authentication.getPrincipal().equals("external_admin");
+        }
+
+        @Override
+        public boolean supports(Class<?> authentication) {
+            return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+        }
     }
 }
